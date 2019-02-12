a37f18a1da14489661a2a15a2ef00215c005f5b7
Xichen(''Michelle'')Luo <45216572+luoxich01@users.noreply.github.com>
2019-02-12 13:07:15 -0800

    Update and rename Two-Sum to Two-Sum.py

--- Two-Sum.py
+++ Two-Sum.py
@@ -1,3 +1,7 @@
+# Goal: Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+
 class Solution(object):   
     def twoSum(self, nums, target):
         for i in range(len(nums)):
